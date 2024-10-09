cf4f135528bb0b15788ab7b98ce5c66a2d30801a
Huertz <104065687+Huertz@users.noreply.github.com>
2024-10-09 00:49:50 -0500

    created home, product, pricing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+
 function App() {
-  return <div>World Wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='home'
+          element={<Home />}
+        />
+        <Route
+          path='product'
+          element={<Product />}
+        />
+        <Route
+          path='pricing'
+          element={<Pricing />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
