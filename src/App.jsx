0bc735065a3222daeb0e5b9c7df92b21dc6f7c02
Huertz <104065687+Huertz@users.noreply.github.com>
2024-10-16 00:27:25 -0500

    added city item

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,31 @@ import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const BASE_URL = 'http://localhost:3000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert('There was an error loading data...');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -35,11 +58,21 @@ function App() {
           {/* default route*/}
           <Route
             index
-            element={<CityList />}
+            element={
+              <CityList
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
           />
           <Route
             path='cities'
-            element={<CityList />}
+            element={
+              <CityList
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
           />
           <Route
             path='countries'
