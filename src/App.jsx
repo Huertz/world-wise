7d29b77069a6a25126fb7190d6379c4cb816cb7e
Huertz <104065687+Huertz@users.noreply.github.com>
2025-05-06 00:05:45 -0500

    creating CitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -6,101 +6,71 @@ import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
-import { useState } from 'react';
-import { useEffect } from 'react';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-
-const BASE_URL = 'http://localhost:3000';
+import { CitiesProvider } from './context/CitiesContext';
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert('There was an error loading data...');
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={<Homepage />}
-        />
-        <Route
-          path='product'
-          element={<Product />}
-        />
-        <Route
-          path='pricing'
-          element={<Pricing />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        {/* Nested Routes */}
-        <Route
-          path='app'
-          element={<AppLayout />}
-        >
-          {/* default route*/}
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
           <Route
-            index
-            element={
-              <Navigate
-                replace
-                to='cities'
-              />
-            }
+            path='/'
+            element={<Homepage />}
           />
           <Route
-            path='cities'
-            element={
-              <CityList
-                cities={cities}
-                isLoading={isLoading}
-              />
-            }
+            path='product'
+            element={<Product />}
           />
           <Route
-            path='cities/:id'
-            element={<City />}
+            path='pricing'
+            element={<Pricing />}
           />
           <Route
-            path='countries'
-            element={
-              <CountryList
-                cities={cities}
-                isLoading={isLoading}
-              />
-            }
+            path='/login'
+            element={<Login />}
           />
+          {/* Nested Routes */}
           <Route
-            path='form'
-            element={<Form />}
+            path='app'
+            element={<AppLayout />}
+          >
+            {/* default route*/}
+            <Route
+              index
+              element={
+                <Navigate
+                  replace
+                  to='cities'
+                />
+              }
+            />
+            <Route
+              path='cities'
+              element={<CityList />}
+            />
+            <Route
+              path='cities/:id'
+              element={<City />}
+            />
+            <Route
+              path='countries'
+              element={<CountryList />}
+            />
+            <Route
+              path='form'
+              element={<Form />}
+            />
+          </Route>
+          <Route
+            path='*'
+            element={<PageNotFound />}
           />
-        </Route>
-        <Route
-          path='*'
-          element={<PageNotFound />}
-        />
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
